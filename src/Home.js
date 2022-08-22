1502b0e666b0d260dec14e1703692a487b64248f
Alisson <alisson.silveira@kbase.com.br>
2022-08-21 21:49:21 -0300

    Iniciando sidebar da home

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,26 @@
-import './App.css';
+import './Home.css';
 import { Link } from "react-router-dom"
 
 const Home = () => {
     return (
-        <div className="App">
-            <h1 className='homeImage'>Fake img</h1>
+        <div>
+            <input type="checkbox" id="check"></input>
+
+            <div className="sidebar">
+                <header>Menu</header>
+                <a href='#'>
+                    <span>Home</span>
+                </a>
+                <a href='#' class='active'>
+                    <span>Nova Transação</span>
+                </a>
+                <a href='#' class='active'>
+                    <span>Histórico</span>
+                </a>
+                <a href='#' class='active'>
+                    <span>Dados</span>
+                </a>
+            </div>
         </div>
     )
 }
