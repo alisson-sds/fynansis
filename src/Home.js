60bffc46d51a28fbd98d49c002bf8f7e2f30bd27
Alisson <alisson.silveira@kbase.com.br>
2022-08-25 08:35:38 -0300

    Adicionados blocos de cotação e aplicações

--- src/Home.js
+++ src/Home.js
@@ -18,9 +18,7 @@ const Home = () => {
         <button onClick={toggleMenu} className='btn material-icons' style={{ color: isOpened ? '#244887' : '#cfd3d6' }}>
           menu
         </button>
-        <button onClick={toggleMenu} className='btn material-icons'>
-          account_circle
-        </button>
+        <button className='btn material-icons'>account_circle</button>
       </header>
       <div className='content'>
         {isOpened && (
@@ -31,7 +29,7 @@ const Home = () => {
             </a>
             <a href='#'>
               <i className='material-icons'>paid</i>
-              <span>Transação</span>
+              <span>Ações</span>
             </a>
             <a href='#'>
               <i className='material-icons'>manage_search</i>
@@ -43,21 +41,33 @@ const Home = () => {
             </a>
           </div>
         )}
-        <div className='mainData'>
-          <div className='block'>
-            <i className='material-icons'>percent</i>
-            <span>18,95</span>
-            <hr /> Rendimento
+        <div className='infoBlock'>
+          <div className='mainData'>
+            <div className='block'>
+              <i className='material-icons'>percent</i>
+              <span>18,95</span>
+              <hr /> Rendimento
+            </div>
+            <div className='block'>
+              <i className='material-icons'>payments</i>
+              <span>R$17.000</span>
+              <hr /> Investido
+            </div>
+            <div className='block'>
+              <i className='material-icons'>wallet</i>
+              <span>R$4.000</span>
+              <hr /> Carteira
+            </div>
           </div>
-          <div className='block'>
-            <i className='material-icons'>payments</i>
-            <span>R$17.000</span>
-            <hr /> Investido
-          </div>
-          <div className='block'>
-            <i className='material-icons'>wallet</i>
-            <span>R$4.000</span>
-            <hr /> Carteira
+          <div className='details'>
+            <div className='detailBlock'>
+              <i className='material-icons'>price_change</i>
+              <span>Cotação</span>
+            </div>
+            <div className='detailBlock'>
+              <i className='material-icons'>monetization_ona</i>
+              <span>Aplicações</span>
+            </div>
           </div>
         </div>
       </div>
